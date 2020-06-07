c71f58d99bcd59dcfb626726f48dc752aeb9dcf2
Daniel Kim <berzzubunny@gmail.com>
2020-06-07 23:17:37 +0900

    feat : FastCampus Python Develop School - Django 0605 Homework Thrid Commit : Tutorial 6 - ViewSets & Routers

--- homework/snippets/serializers.py
+++ homework/snippets/serializers.py
@@ -13,15 +13,9 @@ class SnippetSerializer(serializers.Serializer):
     owner = serializers.ReadOnlyField(source='owner.username')
 
     def create(self, validated_data):
-        """
-        Create and return a new `Snippet` instance, given the validated data.
-        """
         return Snippet.objects.create(**validated_data)
 
     def update(self, instance, validated_data):
-        """
-        Update and return an existing `Snippet` instance, given the validated data.
-        """
         instance.title = validated_data.get('title', instance.title)
         instance.code = validated_data.get('code', instance.code)
         instance.linenos = validated_data.get('linenos', instance.linenos)
--- homework/snippets/views.py
+++ homework/snippets/views.py
@@ -1,8 +1,10 @@
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer, UserSerializer
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, viewsets, renderers
 from django.contrib.auth.models import User
 from snippets.permissions import IsOwnerOrReadOnly
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 
 class SnippetList(generics.ListCreateAPIView):
@@ -28,3 +30,23 @@ class UserList(generics.ListAPIView):
 class UserDetail(generics.RetrieveAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class SnippetViewSet(viewsets.ModelViewSet):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly,
+                          IsOwnerOrReadOnly]
+
+    @action(detail=True, renderer_classes=[renderers.StaticHTMLRenderer])
+    def highlight(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
